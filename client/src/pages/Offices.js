477a979860a1dbff916b4f6ed4db93d5f42dedc0
Spencer Bangterer <bangerter.spencer@gmail.com>
2020-04-23 21:15:39 -0600

    Accordion Setup for Office listing and Office Creation. Need to still update State Dropdown

--- client/src/pages/Offices.js
+++ client/src/pages/Offices.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import API from "../utils/API";
-import { Input, TextArea, FormBtn } from "../components/Form";
-
+import { Input, FormBtn } from "../components/Form";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 export default function Offices() {
   const [offices, setOffices] = useState([]);
   const [formObject, setFormObject] = useState({});
@@ -17,11 +19,6 @@ export default function Offices() {
       .catch((err) => console.log(err));
   }
 
-  //insert office
-  const addOffice = (officeData) => {
-    API.insertOffice(officeData).catch((err) => console.log(err));
-  };
-
   //update office
   const updateOffice = (id, officeData) => {
     API.updateOffice(officeData).catch((err) => console.log(err));
@@ -34,11 +31,16 @@ export default function Offices() {
       .catch((err) => console.log(err));
   }
 
+  //sets state for form object for its contents
   function handleInputChange(event) {
     const { name, value } = event.target;
     setFormObject({ ...formObject, [name]: value });
   }
 
+  function clearForm() {
+    document.getElementById("create-course-form").reset();
+  }
+  //Add office when button click
   function handleFormSubmit(event) {
     event.preventDefault();
     if (
@@ -61,6 +63,7 @@ export default function Offices() {
         managementContactPhone: formObject.managementContactPhone,
       })
         .then((res) => loadOffices())
+        .then(clearForm())
         .catch((err) => console.log(err));
     }
   }
@@ -68,67 +71,96 @@ export default function Offices() {
   return (
     <div>
       <h1>Offices</h1>
-      {offices ? (
-        offices.map((office) => <div>{office.name}</div>)
+      {offices.length ? (
+        offices.map((office) => (
+          <Accordion key={office._id}>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                {office.name}
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  {office.address}
+                  <div>
+                    <Button onClick={() => deleteOffice(office._id)}>
+                      Delete
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        ))
       ) : (
         <p>No Results</p>
       )}
+      <Accordion>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            Create Office
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+            <form id="create-course-form">
+          <Input
+            onChange={handleInputChange}
+            name="name"
+            placeholder="Name (required)"
+          />
+          <Input
+            onChange={handleInputChange}
+            name="address"
+            placeholder="Address (required)"
+          />
+          <Input
+            onChange={handleInputChange}
+            name="city"
+            placeholder="City (required)"
+          />
+          {/*NEEDS TO BE A DROPDOWN. WILL DO LATER*/}
+          <Input
+            onChange={handleInputChange}
+            name="state"
+            placeholder="State (required)"
+          />
+          <Input
+            onChange={handleInputChange}
+            name="zip"
+            placeholder="Zip (required)"
+          />
+          <Input
+            onChange={handleInputChange}
+            name="managementContact"
+            placeholder="Managment Contact Name (required)"
+          />
+          <Input
+            onChange={handleInputChange}
+            name="managementContactPhone"
+            placeholder="Managment Contact Phone (required)"
+          />
 
-      <form>
-        <Input
-          onChange={handleInputChange}
-          name="name"
-          placeholder="Name (required)"
-        />
-        <Input
-          onChange={handleInputChange}
-          name="address"
-          placeholder="Address (required)"
-        />
-        <Input
-          onChange={handleInputChange}
-          name="city"
-          placeholder="City (required)"
-        />
-        {/*NEEDS TO BE A DROPDOWN. WILL DO LATER*/}
-        <Input
-          onChange={handleInputChange}
-          name="state"
-          placeholder="State (required)"
-        />
-        <Input
-          onChange={handleInputChange}
-          name="zip"
-          placeholder="Zip (required)"
-        />
-        <Input
-          onChange={handleInputChange}
-          name="managementContact"
-          placeholder="Managment Contact Name (required)"
-        />
-        <Input
-          onChange={handleInputChange}
-          name="managementContactPhone"
-          placeholder="Managment Contact Phone (required)"
-        />
+          <FormBtn
+            disabled={
+              !(
+                formObject.name &&
+                formObject.address &&
+                formObject.city &&
+                formObject.state &&
+                formObject.zip &&
+                formObject.managementContact &&
+                formObject.managementContactPhone
+              )
+            }
+            onClick={handleFormSubmit}
+          >
+            Create New Office
+          </FormBtn>
+        </form>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
 
-        <FormBtn
-          disabled={
-            !(
-              formObject.name &&
-              formObject.address &&
-              formObject.city &&
-              formObject.state &&
-              formObject.zip &&
-              formObject.managementContact &&
-              formObject.managementContactPhone
-            )
-          }
-          onClick={handleFormSubmit}
-        >
-          Create New Office
-        </FormBtn>
-      </form>
     </div>
   );
 }
