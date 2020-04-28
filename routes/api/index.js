f3f97b2ec0aeaf66e4c5303025d4b157269e2c50
Thyago Volpatto <tvolpatto@gmail.com>
2020-04-27 20:36:03 -0600

    Added Employees CRUD

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const officesRoutes = require("./offices");
+const employeesRoutes = require("./employees");
 
 // Office routes
 router.use("/offices", officesRoutes);
+router.use("/employees", employeesRoutes);
 
 module.exports = router;
