ee6f59b19b09186d279614250358f2cc82511b38
westonbmeier <westonbmeier@gmail.com>
2020-05-04 21:35:06 -0600

    finished user.js and reinstalled package-lock.json

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const userRouter = express.Router; 
+const userRouter = express.Router(); 
 const passport = require("passport"); 
 const passportConfig = require("../../passport");
 const JWT = require("jsonwebtoken")
 const User = require("../../models/users")
+
+const signToken = userID => {
+  return JWT.sign(
+    {
+      iss: 'catsolutions',
+      sub: userID
+    },
+    'catsolutions',
+    { expiresIn: '1h' }
+  )
+}
 
 userRouter.post('/register', (req, res) => {
     const { username, password, role } = req.body
@@ -57,3 +68,42 @@ userRouter.post('/register', (req, res) => {
       }
     }
   )
+
+  userRouter.get(
+    '/logout',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      res.clearCookie('access_token')
+      res.json({ user: { username: '', role: '' }, success: true })
+    }
+  )
+
+  userRouter.get(
+    '/admin',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      if (req.user.role === 'admin') {
+        res
+          .status(200)
+          .json({ message: { msgBody: 'You are an admin', msgError: false } })
+      } else {
+        res
+          .status(403)
+          .json({
+            message: { msgBody: "You're not an admin,go away", msgError: true }
+          })
+      }
+    }
+  )
+
+
+  userRouter.get(
+    '/authenticated',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      const { _id, username, role } = req.user
+      res.status(200).json({ isAuthenticated: true, user: { _id, username, role } })
+    }
+  )
+  
+  module.exports = userRouter
